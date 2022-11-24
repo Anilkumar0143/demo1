9abb20d4a96b49a45670a273e93aaf190ba1f76d
Anil Kumar <53567331+Anilkumar0143@users.noreply.github.com>
2022-11-24 12:15:36 +0000

    updated

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let obj = {
     name:"anil",
     age:25,
     email:'anil@gmail.com',
-    phno:23456784
+    phno:23456784,
+    addr:"tpt"
 }
 app.get('/', (req, res) => res.send(obj))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
